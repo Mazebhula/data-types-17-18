4a8eba75efd00bd0602115c71199c08e9672c15c
tmunyai <tmunyai023@student.wethinkcode.co.za>
2024-09-30 13:51:24 +0200

    initial commit

---
+++ data_types.py
@@ -0,0 +1,63 @@
+def boolean():
+    """
+    Question 1 - Boolean
+
+    Using the variable below, give it the value 'True', then print it.
+    """
+    staying_alive = False
+
+    # enter your code here
+
+
+def integer():
+    """
+    Question 2 - Integer
+
+    Create a program to accept two numbers from a user and multiply them, then print the product.
+    """
+    # enter your code here
+
+
+def string():
+    """
+    Question 3 - String
+
+    Assign your name to a variable below and print it.
+    """
+
+    # enter your code here
+
+
+def convert_to_float():
+    """
+    Question 4 - Float
+
+    Convert the following integer to a float then print it.
+    """
+    int_num = 60
+    # enter your code here
+
+
+def all_data_types():
+    """
+    Question 5 - All Data Types
+
+    Output the following sentence using the given variables.
+
+    Welcome to the 2024 WeThinkCode_ where True learning costs R0.00
+    """
+    string_one = "Welcome to "
+    string_two = " WeThinkCode_ where "
+    string_3 = " learning costs R"
+    bool_condition = True
+    int_year = 2024
+    float_cost = 0.00
+
+    # enter your code here
+
+
+
+if __name__ == "__main__":
+    """
+    Run the entire program from here
+    """
